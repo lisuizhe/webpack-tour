28dbe20e5ba7b8469a9df7626e43b8e5bfb37cbe
Suizhe.Li <suizhe.li@outlook.com>
2020-07-10 14:19:54 +0900

    loading iamges

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './assets/img/icon.png';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
   // Lodash, now import by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  //　Add the image to our existing div
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  
+  element.appendChild(myIcon);
 
   return element;
 }
